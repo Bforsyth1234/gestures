11b01b2c0a8aaca970faaf977c8cdd2283a1af43
Brooks Forsyth <brooks.forsyth@gmail.com>
2020-12-22 06:49:06 -0500

    added drag gesture

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Gesture, GestureController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,39 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  @ViewChild('rectangle', { static: true, read: ElementRef }) rectangle: ElementRef;
+  private gesture: Gesture;
+  constructor(private gestureCtrl: GestureController) {}
+  ngOnInit() {
 
+  }
+
+  ngAfterViewInit(): void {
+    this.gesture = this.gestureCtrl.create({
+      gestureName: 'doubleTap',
+      el: this.rectangle.nativeElement,
+      threshold: 0,
+      onStart: () => { this.onStart();},
+      onMove: ev => this.onMoveHandler(ev),
+      onEnd: ev => {this.onEnd()}
+    }, true);
+  
+    this.gesture.enable();
+  }
+
+  private onEnd() {
+    this.rectangle.nativeElement.style.transition = ".3s ease-out";
+    this.rectangle.nativeElement.style.transform = `translate(0, 0)`;  }
+
+
+  private onMoveHandler(ev) {
+    console.log('ev = ');
+    console.log(ev);
+    this.rectangle.nativeElement.style.transform = `translate(${ev.deltaX}px, ${ev.deltaY}px)`;
+  }
+  
+  private onStart() {
+    this.rectangle.nativeElement.style.transition = "none";
+  }
+  
 }
